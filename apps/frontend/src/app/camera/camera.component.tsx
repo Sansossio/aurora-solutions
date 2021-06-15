7b07cae6374c66e8a6bd924d347218c6676d75a4
Julio Sansossio <juliosansossio@gmail.com>
2021-06-15 21:54:03 +0200

    use videoelement to be able to set video box size

--- apps/frontend/src/app/camera/camera.component.tsx
+++ apps/frontend/src/app/camera/camera.component.tsx
@@ -1,27 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { environment } from '../../environments/environment'
+
+import './camera.scss'
 
 // eslint-disable-next-line
 const JSMpeg = require('@cycjimmy/jsmpeg-player')
 
 export function Camera ({ camera }: { camera: any }) {
   const { proxy } = environment.backend
-  const canvasIdCamera = `camera-${camera.name}`
+  const videoWrapper = useRef<any>(null)
 
   useEffect(() => {
     const url = `${proxy}/stream/camera?rtsp=${encodeURIComponent(camera.rtspUrl)}`
-    void new JSMpeg.Player(
-      url,
-      {
-        canvas: document.getElementById(canvasIdCamera)
-      }
-    )
+    void new JSMpeg.VideoElement(videoWrapper.current, url)
   })
 
   return (
     <div>
       <p>Camera: {camera.name}</p>
-      <canvas id={canvasIdCamera}></canvas>
+      {/* <canvas id={canvasIdCamera}></canvas> */}
+      <div className='videoWrapper' ref={videoWrapper}></div>
     </div>
   )
 }
