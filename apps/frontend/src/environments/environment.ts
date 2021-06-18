96ea1194ebd9f5e19d5b07c42c819647b278a972
Julio Sansossio <juliosansossio@gmail.com>
2021-06-18 11:38:36 +0200

    get host from header and serve project with host 0.0.0.0 to be able to expose this to internet

--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -1,10 +1,12 @@
+const { hostname } = window.location
+
 export const environment = {
   production: false,
   backend: {
-    api: 'http://localhost:3333/api',
-    proxy: 'ws://localhost:3001'
+    api: `http://${hostname}:3333/api`,
+    proxy: `ws://${hostname}:3001`
   },
   ws: {
-    cameraEvents: 'http://localhost:3333/cameras'
+    cameraEvents: `http://${hostname}:3333/cameras`
   }
 }
